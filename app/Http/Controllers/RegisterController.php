df07f52c31a65d7c633a27a925349847355b4871
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-10-21 23:37:09 -0300

    Aula 02 - Validações com a classe Validation

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
     public function store()
     {
-        dump('Cheguei no gravar');
+        $validation = Validator::make([
+            'course' => '',
+            'workload' => 10
+        ],
+        [
+            'course' => ['required', 'max:100'],
+            'workload' => ['required', 'integer']
+        ]);
+
+        if ($validation->fails()) {
+            dd($validation);
+        }
+
+        dd('Validou!');
     }
 }
