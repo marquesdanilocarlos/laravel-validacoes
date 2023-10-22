51c4f58de5123cd91568745d913c12ff59ffe71c
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-10-22 00:35:57 -0300

    Aula 06 - Validação utilizando classe especializada de request

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,19 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CourseRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
-    public function store(Request $request)
+    public function store(CourseRequest $request)
     {
-
-        $request->validate([
-            'course' => ['required', 'max:100'],
-            'workload' => ['required', 'integer']
-        ]);
-
         //$data = $request->except('_token');
 
         /*Validator::make(
@@ -24,6 +19,7 @@ class RegisterController extends Controller
             'workload' => ['required', 'integer']
         ])->validate();*/
 
-        dd('Validou!');
+        dump('Validou!');
+        dump($request->all());
     }
 }
